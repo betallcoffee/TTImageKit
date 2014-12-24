67a9648530ba21d0821402118b33c50f5ff1e3f1
liangliang <liangliang0918@126.com>
2014-12-24 15:37:59 +0800

    comment.

--- TTImageKit/TTQCARCameraViewController.h
+++ TTImageKit/TTQCARCameraViewController.h
@@ -12,6 +12,11 @@
 
 typedef BOOL(^TrackerUpdate)(NSString *targetName);
 
+/**
+ UIViewController 子类，加载 TrackersData 数据文件，初始化传入文件名 dataFilename ，nil 表示使用 TrackersData.xml 。
+ 打开摄像头，扫描摄像头拍摄的图片。当扫描到 TrackersData 中包含的图片时，调用初始化时传入的 trackerUpdate code block 。 传出图片的名字。
+ */
+
 @interface TTQCARCameraViewController : UIViewController<TTQCARControl>
 
 - (instancetype)initWithTrackersDataFilename:(NSString *)dataFilename trackerUpdate:(TrackerUpdate)trackerUpdate;
