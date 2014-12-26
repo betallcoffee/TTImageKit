48e80facde1258501b134a8b1aa69bc36f8f7e4f
liangliang <liangliang0918@126.com>
2014-12-26 15:27:15 +0800

    create TTImageKit library project.

---
+++ TTImageKit/TTImageKit/TTImageKit.h
@@ -0,0 +1,13 @@
+//
+//  TTImageKit.h
+//  TTImageKit
+//
+//  Created by liang on 12/26/14.
+//  Copyright (c) 2014 liangliang. All rights reserved.
+//
+
+
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#endif
